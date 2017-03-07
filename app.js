39eeb6b5a172e5ba48bd0dc447909ebd769e0858
Adam McDaniel <akmcdaniel@gmail.com>
2017-03-06 21:27:49 -0600

    update for local environment

--- app.js
+++ app.js
@@ -48,6 +48,6 @@ app.use(indexRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(3000, function(){
     console.log("The YelpCamp Server Has Started!");
 });
